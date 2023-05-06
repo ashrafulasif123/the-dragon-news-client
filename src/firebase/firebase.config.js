1c46eff05c65987d8e04ca6a0b2f8bfcb1eb0d5e
ashrafulasif123 <cseasifiiuc9@gmail.com>
2023-05-05 21:15:22 -0700

    news dragon

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,14 +4,13 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APIKEY,
-  authDomain: import.meta.env.VITE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_PROJECTID,
-  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_APPID
+  apiKey: "AIzaSyAN2mxCDQK3hmc0OsIwodJPyNj4cJ6sdlM",
+  authDomain: "chef-auth-router-client.firebaseapp.com",
+  projectId: "chef-auth-router-client",
+  storageBucket: "chef-auth-router-client.appspot.com",
+  messagingSenderId: "868791039161",
+  appId: "1:868791039161:web:89bc4cfa823c6b6a829768"
 };
 
 // Initialize Firebase
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -40,7 +40,7 @@ const router = createBrowserRouter([
             {
                 path: ':id',
                 element: <Category></Category>,
-                loader: ({params}) => fetch(`https://the-news-dragon-server-jhankarphero.vercel.app/categories/${params.id}`)
+                loader: ({params}) => fetch(`http://localhost:5000/categories/${params.id}`)
             }
         ]
     }, 
@@ -51,7 +51,7 @@ const router = createBrowserRouter([
             {
                 path: ':id',
                 element: <PrivateRoute><News></News></PrivateRoute>,
-                loader: ({params}) => fetch(`https://the-news-dragon-server-jhankarphero.vercel.app/news/${params.id}`)
+                loader: ({params}) => fetch(`https://http://localhost:5000/news/${params.id}`)
             }
         ]
     }
